cb8db40dd80140aaa34a2dcae669a04c27077244
shrawanc911 <shravanc911@gmail.com>
2025-07-25 21:52:20 +0530

    ENV issue solved

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6X6uRpuJ4DCzkgYpXEzuN6zRXTR74fVs",
-  authDomain: "aarthik-c3351.firebaseapp.com",
-  projectId: "aarthik-c3351",
-  storageBucket: "aarthik-c3351.firebasestorage.app",
-  messagingSenderId: "644240507088",
-  appId: "1:644240507088:web:2adf8af70daabde9091ed5",
-  measurementId: "G-JS6VK1E8J3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
